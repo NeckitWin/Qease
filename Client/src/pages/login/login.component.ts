3387589453691b56c2efa776cd413c200b279c40
NeckitWin <neckit.v@gmail.com>
2025-02-12 01:24:34 +0100

    update: styles and added color variables

--- Client/src/pages/login/login.component.ts
+++ Client/src/pages/login/login.component.ts
@@ -1,12 +1,82 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {FormBuilder, FormGroup, ReactiveFormsModule} from '@angular/forms';
+import {NgIf} from '@angular/common';
+
+const apiUrl: string = 'http://localhost:3000';
+
+interface apiData {
+  data: string;
+  error?: string;
+}
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  imports: [
+    ReactiveFormsModule,
+    NgIf
+  ],
+  templateUrl: './login.component.html'
 })
 export class LoginComponent {
+  errorLogin: string = '';
+  errorRegister: string = '';
+  loginForm: FormGroup;
+  registerForm: FormGroup;
 
+  constructor(private fb: FormBuilder, private http: HttpClient) {
+    this.loginForm = this.fb.group({
+      username: [''],
+      password: ['']
+    });
+
+    this.registerForm = this.fb.group({
+      username: [''],
+      firstPassword: [''],
+      secondPassword: [''],
+      email: [''],
+      acceptTerms: ['']
+    });
+  }
+
+  login() {
+    if (this.loginForm.valid) {
+      const {username, password} = this.loginForm.value;
+      if (!username || !password) {
+        this.errorLogin = 'Wypełnij wszystkie pola';
+        return;
+      }
+      const loginUser = {username, password};
+      this.http.post<apiData>(`${apiUrl}/login`, loginUser).subscribe((response) => {
+        if (response.error) {
+          this.errorLogin = response.error;
+        } else if (response.data) {
+          this.errorLogin = 'Witaj ' + response.data;
+        }
+      });
+    }
+  }
+
+  register() {
+    if (this.registerForm.valid) {
+      const {username, firstPassword, secondPassword, email, acceptTerms} = this.registerForm.value;
+      if (!(username && firstPassword && secondPassword && email && acceptTerms)) {
+        this.errorRegister = 'Wypełnij wszystkie pola';
+        return;
+      }
+      if (firstPassword !== secondPassword) {
+        this.errorRegister = 'Hasła nie są takie same';
+        return;
+      }
+      const registerUser = {username, password: firstPassword, email};
+      this.http.post<apiData>(`${apiUrl}/register`, registerUser).subscribe((response) => {
+        if (response.error) {
+          this.errorRegister = response.error;
+        } else if (response.data) {
+          this.errorRegister = 'Witaj ' + response.data;
+        }
+      });
+    }
+  }
 }
--- Server/index.js
+++ Server/index.js
@@ -1,18 +1,32 @@
 import express from "express";
 import cors from "cors";
 import userRepository from "./database/repository/userReposytory.js";
+import cookieParser from 'cookie-parser';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
+const secretKey = process.env.SECRET_KEY;
 
 app.post("/login", async (req, res) => {
     const user = req.body;
-    const {password} = user;
+    const {username, password} = user;
     const loginUser = await userRepository.getUser(user);
     if (!loginUser) return res.json({error: "Użytkownik nie istnieje"});
     if (loginUser.password !== password) return res.json({error: "Nieprawidłowe hasło"});
-    res.json({data: loginUser.username});
+    const token = jwt.sign({username}, secretKey, {expiresIn: "1h"});
+
+    res.cookie("authToken", token, {
+        httpOnly: true,
+        sameSite: "Strict",
+        maxAge: 3600000
+    });
+
+    res.json({message: 'Udane logowanie!'});
 });
 
 app.post("/register", async (req, res) => {
@@ -22,8 +36,15 @@ app.post("/register", async (req, res) => {
         return res.json({error: "Użytkownik już istnieje"});
     }
     const newUser = await userRepository.postUser(user);
-    res.json({data: newUser.username});
+    const token = jwt.sign({username: newUser.username}, secretKey, {expiresIn: "1h"});
 
+    res.cookie("authToken", token, {
+        httpOnly: true,
+        sameSite: "Strict",
+        maxAge: 3600000
+    });
+
+    res.json({message: 'Udane zarejestrowanie!'});
 });
 
 app.listen(3000, () => {
